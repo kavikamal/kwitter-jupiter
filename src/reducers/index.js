411b2265cde7650577d7b56fad6fd9dcf6bb5fe2
Christine Ooi <christine.ooi@gmail.com>
2018-06-20 07:42:07 -0400

    Added semantic ui react as dependency, modified LoginUser component to connect to store, modified loginUser action to fetch from api

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,19 @@
-import { LOGIN_USER } from '../actions';
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from '../actions';
 
 const initialState = {
-
+    error: '',
+    message: '',
+    isLoggedIn: false
 }
 
 export default (state = initialState, action) => {
     switch (action.type) {
-        case LOGIN_USER:
-
-            break;
+        case AUTH_USER:
+            return { ...state, error: '', message: '', isLoggedIn: true };
+        case UNAUTH_USER:
+            return { ...state, isLoggedIn: false };
+        case AUTH_ERROR:
+            return { ...state, error: action.payload };
         default:
             return state;
     }
