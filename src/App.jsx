3e5e97243ea1fb8f45a1a2afc85c461701aa0645
Colin Bruin <colinjb23@gmail.com>
2018-06-21 10:04:25 -0400

    added the message component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import LoginUser from './components/LoginUser';
+import Messages from './components/Messages';
 
 class App extends Component {
   render() {
     return (
       <div>
         <LoginUser />
+        <Messages />
       </div>
     );
   }
