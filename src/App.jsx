0a791158c9a84afaf7df667af14e0d80a4f9aa5f
Colin Bruin <colinjb23@gmail.com>
2018-06-21 11:50:53 -0400

    need to add the connect but got the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import LoginUser from './components/LoginUser';
 import Messages from './components/Messages';
-
+export const ADD_MESSAGES = 'ADD_MESSAGES';
 class App extends Component {
+  
+  componentDidMount() {
+    fetch('https://kwitter-api.herokuapp.com/messages')
+      .then(response => response.json())
+      .then(data => {
+        dispatch({
+          type: ADD_MESSAGES,
+          messages: data
+        })
+      })
+  }
+
   render() {
     return (
       <div>
@@ -14,3 +26,5 @@ class App extends Component {
 }
 
 export default App;
+
+// Need to map state to props then do this.props.dispatch
