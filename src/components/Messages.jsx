8198806ece9ea3977c5b70cb25869da3dadc3705
Colin Bruin <colinjb23@gmail.com>
2018-06-22 09:54:49 -0400

    showing the messages

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import '../App.css';
 
 
 class Messages extends React.Component {
     render () {
+        // Need to find away to reduce this
         const messagesToMap = this.props.messages.messageReducer.messages.messages;
         return (
             <div>
                 <ul>
                 {
                 messagesToMap.map((item, i) => {
-                    console.log(item.text)
-                    return <li key={i}>{item.text}</li>
+                    console.log(item)
+                    return (
+                        <li key={i} className="messageItem">{item.text}</li>
+                    )
                 })}
                 </ul>
             </div>        
